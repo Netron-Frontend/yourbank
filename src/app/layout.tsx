5aa76aeebe2d8acb5d3ddb2a5e02afa4821d4b95
Ivan <ivanmakarevic3@gmail.com>
2025-08-06 00:15:27 +0300

    Revert "create a NavBar"
    This reverts commit 6d2899b9212ff5d3995e2cd2429be8a99f964a11.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next';
-import '@/styles/global.css';
-import '@/styles/theme.css';
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: 'YourBanK',
-	description: 'Created by Ivan Makarevich'
+  title: "YourBanK",
+  description: "Created by Ivan Makarevich",
 };
 
 export default function RootLayout({
-	children
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<body>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,8 @@
-'use client';
 
-import { MainPage, PageContainer } from './page.styled';
-import Image from 'next/image';
-import AbstractDesign1 from '../../public/images/MainPage/Abstract Design-1.png';
-import NavBar from '@/components/HomePage/NavBar/NavBar';
 
-export default function Page() {
-	return (
-		<MainPage>
-			<Image
-				src={AbstractDesign1}
-				alt={'Abstract Design-1'}
-				style={{
-					width: 759,
-					height: 610.81,
-					left: '-70px',
-					top: '-120px',
-					position: 'absolute',
-					zIndex: 900
-				}}
-			/>
-			<PageContainer style={{ position: 'relative', zIndex: 1000 }}>
-				<NavBar />
-			</PageContainer>
-		</MainPage>
-	);
+export default function Home() {
+  return (
+  <>
+  </>
+  );
 }
